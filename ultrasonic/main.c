b52b75aeac37947a0d377e72072ac761d5b29ccc
Pairode Jaroensri <p.k.jaroensri@berkeley.edu>
2018-11-05 23:30:50 -0800

    minor changes

--- ultrasonic/main.c
+++ ultrasonic/main.c
@@ -25,7 +25,6 @@
 
 // Ultrasonic time keeping
 static const nrf_drv_timer_t US_timer = NRFX_TIMER_INSTANCE(1);
-//  static const nrf_drv_timer_t US_disable_timer = NRFX_TIMER_INSTANCE(1);
 static const nrf_drv_timer_config_t timer_cfg = {
     .frequency          = NRF_TIMER_FREQ_1MHz,
     .mode               = NRF_TIMER_MODE_TIMER,
@@ -34,8 +33,8 @@ static const nrf_drv_timer_config_t timer_cfg = {
     .p_context          = NULL,
 };
 
-static uint32_t disable_interval = 0;
-static uint32_t detection_interval = 0;
+static uint32_t US_disable_interval = 0;
+static uint32_t US_detection_interval = 0;
 
 static uint32_t US_time[3] = {0, 0, 0};
 static uint32_t time_offset01, time_offset02, time_offset12;
@@ -51,24 +50,25 @@ void GPIOTE_IRQHandler(void) {
         NRF_GPIOTE->INTENCLR = 1;
         US_time[0] = nrfx_timer_capture(&US_timer, NRF_TIMER_CC_CHANNEL0);
         last_detection = US_timer[0];
-        nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL0, US_time[0] + disable_interval, 1);
+        nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL0, US_time[0] + US_disable_interval, 1);
     }
     if (NRF_GPIOTE->EVENTS_IN[1]) {
         NRF_GPIOTE->EVENTS_IN[1] = 0;
         NRF_GPIOTE->INTENCLR = 2;
         US_time[1] = nrfx_timer_capture(&US_timer, NRF_TIMER_CC_CHANNEL0);
         last_detection = US_timer[1];
-        nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL1, US_time[1] + disable_interval, 1);
+        nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL1, US_time[1] + US_disable_interval, 1);
     }
     if (NRF_GPIOTE->EVENTS_IN[2]) {
         NRF_GPIOTE->EVENTS_IN[2] = 0;
         NRF_GPIOTE->INTENCLR = 4;
         US_time[2] = nrfx_timer_capture(&US_timer, NRF_TIMER_CC_CHANNEL0);
         last_detection = US_timer[2];
-        nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL2, US_time[2] + disable_interval, 1);
+        nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL2, US_time[2] + US_disable_interval, 1);
     }
 
-    nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL3, last_detection + detection_interval, 1);
+    // Delay before calculating time offset to make sure all 3 sensors have a chance to log detection.
+    nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL3, last_detection + US_detection_interval, 1);
 
     //__disable_irq();
     // calculate_time_offset();
@@ -78,6 +78,7 @@ void GPIOTE_IRQHandler(void) {
 
 void calculate_time_offset(void) {
     printf("US0: %d, US1: %d, US2: %d\n", US_time[0]/1000000, US_time[1]/1000000, US_time[2]/1000000);
+    US_time = {0, 0, 0};
 }
 
 static void US_timer_event_handler(nrf_timer_event_t event_type, void* p_context) {
@@ -110,9 +111,9 @@ int main(void) {
   printf("Log initialized!\n");
 
   // Set LED for debugging
-  nrf_gpio_pin_dir_set(BUCKLER_LED0, NRF_GPIO_PIN_DIR_OUTPUT);
-  nrf_gpio_pin_dir_set(BUCKLER_LED1, NRF_GPIO_PIN_DIR_OUTPUT);
-  nrf_gpio_pin_dir_set(BUCKLER_LED2, NRF_GPIO_PIN_DIR_OUTPUT);
+  // nrf_gpio_pin_dir_set(BUCKLER_LED0, NRF_GPIO_PIN_DIR_OUTPUT);
+  // nrf_gpio_pin_dir_set(BUCKLER_LED1, NRF_GPIO_PIN_DIR_OUTPUT);
+  // nrf_gpio_pin_dir_set(BUCKLER_LED2, NRF_GPIO_PIN_DIR_OUTPUT);
 
   // Initialize interrupts
   NRF_GPIOTE->CONFIG[0] = MODE_EVENT | US0_PIN << PSEL_POS | POLARITY_LOTOHI << POLARITY_POS;
@@ -126,14 +127,10 @@ int main(void) {
   // Initialize timer
   error_code = nrfx_timer_init(&US_timer, &timer_cfg, US_timer_event_handler);
   APP_ERROR_CHECK(error_code);
-  // error_code = nrfx_timer_init(&US_disable_timer, &timer_cfg, US_disable_timer_event_handler);
-  // APP_ERROR_CHECK(error_code);
-  disable_interval = nrfx_timer_ms_to_ticks(&US_timer, 20);
-  detection_interval = nrfx_timer_us_to_ticks(&US_timer, 1000);
+  US_disable_interval = nrfx_timer_ms_to_ticks(&US_timer, 20);
+  US_detection_interval = nrfx_timer_us_to_ticks(&US_timer, 1000);
   nrfx_timer_clear(&US_timer);
   nrfx_timer_enable(&US_timer);
-
-  
 
   // loop forever
   while (1) {
