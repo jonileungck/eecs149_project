182cbd84f5e1aa832f6c1dd7122eb56efbb737f4
Pairode Jaroensri <p.k.jaroensri@berkeley.edu>
2018-11-07 18:46:24 -0800

    ultrasonic works now

---
+++ software/apps/beacon/README.md
@@ -0,0 +1,14 @@
+Ultrasonic Beacon  
+==============
+
+Gives a trigger pulse to drive the Grove Ultrasonic range finder and ignore the response.  
+The trigger signal goes out through pins A0, D0, SCL, and TX all at the same time.  
+
+If SWITCH0 is set, the Buckler waits for a button press to send out a trigger pulse.  
+If SWITCH0 is clear, the Buckler sends out a pulse every 100ms.  
+
+Detection range is severely reduced when the reflector cone is installed:  
+
+Range when reflector on both receiver and transmitter: 1m  
+Range when reflector on receiver only: 2m  
+Range when no reflector: >4m  
--- software/apps/ultrasonic/main.c
+++ software/apps/ultrasonic/main.c
@@ -13,7 +13,6 @@
 #include "nrf_pwr_mgmt.h"
 #include "nrf_serial.h"
 #include "nrf_drv_gpiote.h"
-#include "nrf_drv_timer.h"
 
 #include "buckler.h"
 #include "gpiote.h"
@@ -23,83 +22,48 @@
 #define US1_PIN BUCKLER_GROVE_A1
 #define US2_PIN BUCKLER_GROVE_D1
 
-// Ultrasonic time keeping
-static const nrf_drv_timer_t US_timer = NRFX_TIMER_INSTANCE(1);
-static const nrf_drv_timer_config_t timer_cfg = {
-    .frequency          = NRF_TIMER_FREQ_1MHz,
-    .mode               = NRF_TIMER_MODE_TIMER,
-    .bit_width          = NRF_TIMER_BIT_WIDTH_32,
-    .interrupt_priority = NRFX_TIMER_DEFAULT_CONFIG_IRQ_PRIORITY,
-    .p_context          = NULL,
-};
 
-static uint32_t US_disable_interval = 1;
-static uint32_t US_detection_interval = 0;
+static uint32_t US_disable_interval = 35000;
+static uint32_t US_detection_interval = 1000;
 
-static uint32_t US_time[3] = {0, 0, 0};
+static uint32_t US_times[3] = {0, 0, 0};
 static uint32_t time_offset01, time_offset02, time_offset12;
 
 static int counts = 0;
-// void GPIOTE_IRQHandler(void) {
-//     uint32_t last_detection;
-//     if (NRF_GPIOTE->EVENTS_IN[0]) {
-//         NRF_GPIOTE->EVENTS_IN[0] = 0;
-//         NRF_GPIOTE->INTENCLR = 1;
-//         US_time[0] = nrfx_timer_capture(&US_timer, NRF_TIMER_CC_CHANNEL0);
-//         last_detection = US_time[0];
-//         nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL0, US_time[0] + US_disable_interval, 1);
-//     }
-//     if (NRF_GPIOTE->EVENTS_IN[1]) {
-//         NRF_GPIOTE->EVENTS_IN[1] = 0;
-//         NRF_GPIOTE->INTENCLR = 2;
-//         US_time[1] = nrfx_timer_capture(&US_timer, NRF_TIMER_CC_CHANNEL1);
-//         last_detection = US_time[1];
-//         nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL1, US_time[1] + US_disable_interval, 1);
-//     }
-//     if (NRF_GPIOTE->EVENTS_IN[2]) {
-//         NRF_GPIOTE->EVENTS_IN[2] = 0;
-//         NRF_GPIOTE->INTENCLR = 4;
-//         US_time[2] = nrfx_timer_capture(&US_timer, NRF_TIMER_CC_CHANNEL2);
-//         last_detection = US_time[2];
-//         nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL2, US_time[2] + US_disable_interval, 1);
-//     }
 
-//     // Delay before calculating time offset to make sure all 3 sensors have a chance to log detection.
-//     nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL3, last_detection + US_detection_interval, 1);
-
-//     //__disable_irq();
-//     // calculate_time_offset();
-//     //__enable_irq();
-
-// }
-
-void calculate_time_offset(void) {
-    printf("%d: US0: %d, US1: %d, US2: %d\n", counts, US_time[0]/1000000, US_time[1]/1000000, US_time[2]/1000000);
-    counts += 1;
-    // US_time[0] = 0;
-    // US_time[1] = 0;
-    // US_time[2] = 0;
+// Read the current value of the timer counter
+uint32_t read_timer(int channel) {
+  // Should return the value of the internal counter for TIMER1
+  NRF_TIMER1->TASKS_CAPTURE[channel] = 1;
+  return NRF_TIMER1->CC[channel];
 }
 
-static void US_timer_event_handler(nrf_timer_event_t event_type, void* p_context) {
-  // turn on interrupt
-    printf("Timer event in\n");
-    if (NRF_TIMER0->EVENTS_COMPARE[0]) {
-        NRF_TIMER0->EVENTS_COMPARE[0] = 0;
+void calculate_time_offset(void) {
+    printf("%d: US0: %d, US1: %d, US2: %d\n", counts, US_times[0], US_times[1], US_times[2]);
+    counts += 1;
+    // US_times[0] = 0;
+    // US_times[1] = 0;
+    // US_times[2] = 0;
+}
+
+void TIMER1_IRQHandler(void) {
+  // turn on interrupt for US detectors
+    if (NRF_TIMER1->EVENTS_COMPARE[0]) {
+        NRF_TIMER1->EVENTS_COMPARE[0] = 0;
+        NRF_TIMER1->INTENCLR = 1 << 16;
         // NRF_GPIOTE->INTENSET = 1;
-        printf("Timer US0\n");
         nrfx_gpiote_in_event_enable(US0_PIN, true);
     }
     if (NRF_TIMER1->EVENTS_COMPARE[1]) {
         NRF_TIMER1->EVENTS_COMPARE[1] = 0;
-        // NRF_GPIOTE->INTENSET = 1 << 1;
-        printf("Timer US1\n");
+        NRF_TIMER1->INTENCLR = 1 << 17;
+        // NRF_GPIOTE->INTENSET = 1;
         nrfx_gpiote_in_event_enable(US1_PIN, true);
     }
     if (NRF_TIMER1->EVENTS_COMPARE[2]) {
         NRF_TIMER1->EVENTS_COMPARE[2] = 0;
-        // NRF_GPIOTE->INTENSET = 1 << 2;
-        printf("Timer US2\n");
+        NRF_TIMER1->INTENCLR = 1 << 18;
+        // NRF_GPIOTE->INTENSET = 1;
         nrfx_gpiote_in_event_enable(US2_PIN, true);
     }
     if (NRF_TIMER1->EVENTS_COMPARE[3]) {
@@ -111,33 +75,36 @@ static void US_timer_event_handler(nrf_timer_event_t event_type, void* p_context
 void US0_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action) {
     // NRF_GPIOTE->INTENCLR = 1;
     nrfx_gpiote_in_event_disable(US0_PIN);
-    US_time[0] = nrfx_timer_capture(&US_timer, NRF_TIMER_CC_CHANNEL0);
-    nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL0, US_time[0] + US_disable_interval, true);
-    nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL3, US_time[0] + US_detection_interval, true);
+    US_times[0] = read_timer(0);
+    NRF_TIMER1->INTENSET = 1 << 16;
+    NRF_TIMER1->CC[0] = US_times[0] + US_disable_interval;
+    NRF_TIMER1->CC[3] = US_times[0] + US_detection_interval;
     nrf_drv_gpiote_out_toggle(BUCKLER_LED0);
-    printf("US0 triggered\n");
+    printf("US0 triggered at %i\n", US_times[0]);
     // calculate_time_offset();
 }
 
 void US1_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action) {
-    // NRF_GPIOTE->INTENCLR = 1 << 1;
+    // NRF_GPIOTE->INTENCLR = 1;
     nrfx_gpiote_in_event_disable(US1_PIN);
-    US_time[1] = nrfx_timer_capture(&US_timer, NRF_TIMER_CC_CHANNEL1);
-    nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL1, US_time[1] + US_disable_interval, true);
-    nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL3, US_time[1] + US_detection_interval, true);
+    US_times[1] = read_timer(1);
+    NRF_TIMER1->INTENSET = 1 << 17;
+    NRF_TIMER1->CC[1] = US_times[1] + US_disable_interval;
+    NRF_TIMER1->CC[3] = US_times[1] + US_detection_interval;
     nrf_drv_gpiote_out_toggle(BUCKLER_LED1);
-    printf("US1 triggered\n");
+    printf("US1 triggered at %i\n", US_times[1]);
     // calculate_time_offset();
 }
 
 void US2_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action) {
-    // NRF_GPIOTE->INTENCLR = 1 << 2;
+    // NRF_GPIOTE->INTENCLR = 1;
     nrfx_gpiote_in_event_disable(US2_PIN);
-    US_time[2] = nrfx_timer_capture(&US_timer, NRF_TIMER_CC_CHANNEL2);
-    nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL2, US_time[2] + US_disable_interval, true);
-    nrfx_timer_compare(&US_timer, NRF_TIMER_CC_CHANNEL3, US_time[2] + US_detection_interval, true);
+    US_times[2] = read_timer(2);
+    NRF_TIMER1->INTENSET = 1 << 18;
+    NRF_TIMER1->CC[2] = US_times[2] + US_disable_interval;
+    NRF_TIMER1->CC[3] = US_times[2] + US_detection_interval;
     nrf_drv_gpiote_out_toggle(BUCKLER_LED2);
-    printf("US2 triggered\n");
+    printf("US2 triggered at %i\n", US_times[2]);
     // calculate_time_offset();
 }
 
@@ -159,7 +126,7 @@ static void gpio_init(void) {
 
     // Init ultrasonic
     nrf_drv_gpiote_in_config_t in_config = GPIOTE_CONFIG_IN_SENSE_LOTOHI(true);
-    // in_config.pull = NRF_GPIO_PIN_PULLUP;
+    in_config.pull = NRF_GPIO_PIN_NOPULL;
 
     err_code = nrf_drv_gpiote_in_init(US0_PIN, &in_config, US0_handler);
     APP_ERROR_CHECK(err_code);
@@ -168,9 +135,9 @@ static void gpio_init(void) {
     err_code = nrf_drv_gpiote_in_init(US2_PIN, &in_config, US2_handler);
     APP_ERROR_CHECK(err_code);
 
-    nrf_drv_gpiote_in_event_enable(US0_PIN, true);
-    nrf_drv_gpiote_in_event_enable(US1_PIN, true);
-    nrf_drv_gpiote_in_event_enable(US2_PIN, true);
+    nrfx_gpiote_in_event_enable(US0_PIN, true);
+    nrfx_gpiote_in_event_enable(US1_PIN, true);
+    nrfx_gpiote_in_event_enable(US2_PIN, true);
 }
 
 int main(void) {
@@ -197,12 +164,15 @@ int main(void) {
   gpio_init();
 
   // Initialize timer
-  error_code = nrfx_timer_init(&US_timer, &timer_cfg, US_timer_event_handler);
-  APP_ERROR_CHECK(error_code);
-  US_disable_interval = nrfx_timer_ms_to_ticks(&US_timer, 20);
-  US_detection_interval = nrfx_timer_us_to_ticks(&US_timer, 1000);
-  nrfx_timer_clear(&US_timer);
-  nrfx_timer_enable(&US_timer);
+  NVIC_EnableIRQ(TIMER1_IRQn);
+  
+  NRF_TIMER1->BITMODE = 3;
+  NRF_TIMER1->PRESCALER = 4;
+  NRF_TIMER1->INTENSET = 1 << 19; // Enable compare for CC3
+
+  NRF_TIMER1->TASKS_STOP = 1;
+  NRF_TIMER1->TASKS_CLEAR = 1;
+  NRF_TIMER1->TASKS_START = 1;
 
   // loop forever
   while (1) {
